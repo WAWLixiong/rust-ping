725e08643264074a8a24074a61e45851ca88178e
WAWLixiong <17601241441@163.com>
2021-07-16 23:43:46 +0800

    出初步完基本功能完å成

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 mod utils;
 
-// use std::net::{Ipv4Addr, IpAddr};
 use std::time::Duration;
 use std::path::Path;
 use std::fs;
 use clap::{AppSettings, Clap};
 use rand::random;
 use ping;
-// use anyhow::{Result, anyhow};
-// use std::fmt::Display;
 
 /// this is a tool for ip ping batch testing
 #[derive(Clap)]
@@ -17,38 +14,27 @@ use ping;
 struct Opts {
     /// concurrent num
     #[clap(short, long, default_value = "4")]
-    thread_num: u8,
+    thread_num: usize,
     /// ip separate with ip or one ip file per line
     ips: String,
 }
 
-fn check_ip(ip: &str) {
-    // fn to_anyhow(e: impl Display) -> anyhow::Error {
-    //     anyhow!(e.to_string())
-    // }
-    // let ip = Ipv4Addr::from_str(ip).unwrap();
+fn ping_ip(ip: &str){
     let ret = ping::ping(ip.parse().unwrap(), Some(Duration::from_secs(1)), Some(166), Some(3), Some(5), Some(&random()));
-    // .map_err(to_anyhow);
     match ret {
         Ok(_) => println!("{} yes", ip),
         Err(_) => println!("{} no", ip)
     }
+    // todo: 在线程池内检测时结果会不准确， 需要研究下ping的机制
 }
 
 fn check_is_file(file_name: &str) -> bool {
     let file_path = format!("./{}", file_name);
-    let path = Path::new(file_path.as_str());
-    let parent = path.parent().and_then(|p| p.is_dir().then(|| p));
-    match parent {
-        Some(_) => true,
-        None => false,
-    }
+    Path::new(file_path.as_str()).exists()
 }
 
 fn read_file(ips: &str) -> Vec<String> {
     let file_path = format!("./{}", ips);
-    // let file_name= file_name.as_str();
-    // let file_path = Path::new(file_name);
     let result = fs::read_to_string(file_path).unwrap();
     result.split("\r\n").map(|s| s.to_string()).collect()
 }
@@ -61,15 +47,39 @@ fn parse_ip(ips: &str) -> Vec<String> {
     }
 }
 
+fn parse_thread_num(thread_nums: usize, ip_nums: usize) -> usize {
+    let mut nums = thread_nums;
+    if ip_nums < thread_nums {
+        nums = ip_nums
+    };
+    if nums < 1 {
+        nums = 1
+    };
+    if nums > 20 {
+        nums = 20
+    }
+    eprintln!("Concurrent num is {}", nums);
+    nums
+}
 
 fn main() {
-    // let opts: Opts = Opts::parse();
-    // let a = "220.181.38.148";
-    // check_ip(a);
-    // let file_name = "text";
-    // let ret = check_is_file(file_name);
-    // println!("ret is {}", ret);
+    let opts: Opts = Opts::parse();
+    let ips = opts.ips;
+    let ips = parse_ip(&ips);
+    let ip_nums = ips.len();
+    let size = parse_thread_num(opts.thread_num, ip_nums);
+    let threadpool = utils::threadpool::ThreadPool::new(size);
 
-    let vec = parse_ip("test");
-    println!("{:?}", vec);
+    for ip in ips.into_iter() {
+        threadpool.execute(move || ping_ip(ip.as_str()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        ping_ip("11.11.11.11");
+    }
 }
--- src/utils/threadpool.rs
+++ src/utils/threadpool.rs
@@ -1,7 +1,8 @@
-use std::thread::{Thread, JoinHandle};
+use std::thread::JoinHandle;
 use std::sync::mpsc::{Sender, Receiver};
-use std::sync::Mutex;
+use std::sync::{Mutex, mpsc};
 use std::sync::Arc;
+use std::thread;
 
 enum Message {
     NewJob(Job),
@@ -15,6 +16,7 @@ pub struct ThreadPool {
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+#[allow(dead_code)]
 struct Worker {
     id: usize,
     thread: Option<JoinHandle<()>, >,
@@ -24,12 +26,12 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-        let (sender, receiver) = mspc::channel();
-        let receiver = Arc::new(Mutext::new(receiver));
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
 
         let mut workers = Vec::with_capacity(size);
         for id in 0..size {
-            workers.push(Worker::new(id, receiver::clone(&receiver)));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
         ThreadPool {
             workers,
@@ -42,22 +44,17 @@ impl ThreadPool {
             F: FnOnce() + Send + 'static
     {
         let job = Box::new(f);
-        self.sender.sned(Message::NewJob(job)).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
-        println!("Sending terminate message to all workers.");
-
-        for _ in &mut self.workers {
+        for _ in &self.workers {
             self.sender.send(Message::Terminate).unwrap();
         }
 
-        println!("Shutting down all workers.");
-
         for worker in &mut self.workers {
-            println!("Shutind down worker {}", worker.id);
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
             }
@@ -67,16 +64,14 @@ impl Drop for ThreadPool {
 
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<Receiver<Message>>>) -> Self {
-        let thread = Thread::spawn(move || {
+        let thread = thread::spawn(move || {
             loop {
-                let messsage = receiver.lock().unwrap().recv().unwrap();
+                let message = receiver.lock().unwrap().recv().unwrap();
                 match message {
                     Message::NewJob(job) => {
-                        println!("Worker {} got a job; executing.", id);
                         job();
                     },
                     Message::Terminate => {
-                        println!("Worker {} was told to terminate.", id);
                         break;
                     }
                 }
